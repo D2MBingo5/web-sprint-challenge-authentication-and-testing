975e8a945c9962686dbc7c9f33c5dfdd18e77fa4
Leu Family <romanianfobjr@gmail.com>
2021-09-17 13:52:36 -0500

    intuitive tests written

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,19 @@
-// Write your tests here
+const Auth = require('./auth/auth-router')
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).not.toBe(false)
+})
+
+describe('register user', () => {
+  test('successful registration responds with newly created user', () =>{
+
+  })
+  test.todo('responds with correct body if missing username or password')
+  test.todo('responds with correct body if username taken')
+})
+
+describe('login user', () => {
+  test.todo('successful registration contains token in body')
+  test.todo('responds with correct body if missing username or password')
+  test.todo('responds with correct body if UN/PW is incorrect')
 })
