465f3dc73e7f03c43b26639d1def1ba06498f154
Leu Family <romanianfobjr@gmail.com>
2021-09-17 14:23:58 -0500

    test 1, happy path registration complete

--- api/server.test.js
+++ api/server.test.js
@@ -1,19 +1,43 @@
-const Auth = require('./auth/auth-router')
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+afterAll(async () => {
+  await db.destroy()
+})
 
 test('sanity', () => {
   expect(true).not.toBe(false)
 })
 
 describe('register user', () => {
-  test('successful registration responds with newly created user', () =>{
 
+  test('successful registration responds with newly created user', async () => {
+    const res = await request(server).post('/api/auth/register').send({
+      username: 'superman',
+      password: 'super'
+    })
+    expect(res.status).toBe(201)
+    expect(res.body).toMatchObject({ id: 1,
+    username: 'superman' })
   })
+
   test.todo('responds with correct body if missing username or password')
+
   test.todo('responds with correct body if username taken')
+
 })
 
 describe('login user', () => {
+
   test.todo('successful registration contains token in body')
+
   test.todo('responds with correct body if missing username or password')
+
   test.todo('responds with correct body if UN/PW is incorrect')
+
 })
---
+++ api/users/users-model.js
@@ -0,0 +1,17 @@
+const db = require('../../data/dbConfig')
+
+function findById(id) {
+    return db('users')
+    .select('id', 'username', 'password')
+    .where({ id }).first()
+}
+
+async function add(user) {
+    const [id] = await db('users').insert(user, 'id')
+    return findById(id)
+}
+
+module.exports = {
+    add,
+    findById
+}
